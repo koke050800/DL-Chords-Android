b82e2b8d72b105cafc6a11f747efa1b5725c8846
CarlitosObrian <68962418+CarlitosObr@users.noreply.github.com>
2022-10-24 18:46:52 -0500

    lista la vista de la pantalla de recorte, a espera de terminar la funcionalidad del boton

--- app/src/main/java/com/example/DLChordsTT/features/audio_list/features/stored_audios_list/features/cut_audio/ui/screens/CutAnAudio.kt
+++ app/src/main/java/com/example/DLChordsTT/features/audio_list/features/stored_audios_list/features/cut_audio/ui/screens/CutAnAudio.kt
@@ -1,6 +1,7 @@
 package com.example.DLChordsTT.features.audio_list.features.stored_audios_list.features.cut_audio.ui.screens
 
 import DLChordsTT.R
+import android.content.Intent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
@@ -8,20 +9,20 @@ import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.composed
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.DLChordsTT.features.audio_list.features.processed_audio_list.data.models.AudioProc
-import com.example.DLChordsTT.features.audio_list.features.processed_audio_list.view_models.AudioProcViewModel
+import androidx.core.content.ContextCompat
 import com.example.DLChordsTT.features.audio_list.features.stored_audios_list.data.models.Audio
+import com.example.DLChordsTT.features.audio_list.features.stored_audios_list.features.recognize_lyric_chords.view_models.PythonFlaskApiViewModel
 import com.example.DLChordsTT.features.audio_list.features.stored_audios_list.view_models.AudioViewModel
-import com.example.DLChordsTT.features.audio_list.ui.components.AlertDialogProcessedAudio
+import com.example.DLChordsTT.features.audio_list.ui.components.AlertDialogProcessing
 import com.example.DLChordsTT.features.audio_list.ui.components.timeStampToDuration
 import com.example.DLChordsTT.features.audio_list.ui.components.timeStampToSeconds
-import com.example.DLChordsTT.features.generated_files.features.file_pdf_list.view_models.GeneratedFilesViewModel
+import com.example.DLChordsTT.features.generated_files.features.file_pdf_list.ui.screens.holiActivity
 import com.example.DLChordsTT.features.music_player.ui.components.TopAppBarPlayer
 import com.example.DLChordsTT.ui.theme.DLChordsTheme
+import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterialApi::class)
 @Composable
@@ -30,11 +31,14 @@ fun CutAnAudioScreen(
     onProgressChange: (Float) -> Unit,
     audio: Audio,
     audioViewModel: AudioViewModel,
-    audioProcViewModel: AudioProcViewModel,
-    generatedFilesViewModel: GeneratedFilesViewModel,
-    isAlreadyProcessed: Boolean,
+    pythonFlaskApiViewModel: PythonFlaskApiViewModel,
 ) {
-    val openDialog = remember { mutableStateOf(false) }
+    val openDialogProcessing = remember { mutableStateOf(false) }
+    var scope = rememberCoroutineScope()
+
+    val context = LocalContext.current
+    val pdfScreenIntent =
+        Intent(context, holiActivity::class.java) // TODO: quitar holis activity y poner la de pdfs
 
     DLChordsTheme {
 
@@ -80,7 +84,7 @@ fun CutAnAudioScreen(
                             thumbColor = DLChordsTheme.colors.secondaryText,
                             activeTrackColor = DLChordsTheme.colors.secondaryText
                         )
-                        )
+                    )
                     RangeSlider(
                         values = select,
                         valueRange = range,
@@ -100,28 +104,21 @@ fun CutAnAudioScreen(
                 }
             }
 
-            AlertDialogProcessedAudio(openDialogProcessedAudio = openDialog)
+            AlertDialogProcessing(openDialogProcessing = openDialogProcessing)
 
             Button(
                 onClick = {
                     var timeInitial = (select.start.toLong() * audio.duration) / 100
                     var timeEnd = (select.endInclusive.toLong() * audio.duration) / 100
                     println("De ${timeStampToSeconds(timeInitial)} a ${timeStampToSeconds(timeEnd)}")
-                    /*if (!isAlreadyProcessed) {
-                        audioProcViewModel.addNewAudioProc(
-                            AudioProc(
-                                id = audio.id,
-                                displayName = audio.displayName,
-                                artist = audio.artist,
-                                data = audio.data,
-                                duration = 344324,
-                                title = audio.title,
 
-                                )
+                    scope.launch {
+                        pythonFlaskApiViewModel.uploadAudioAndCut(
+                            audio,
+                            timeStampToSeconds(timeInitial),
+                            timeStampToSeconds(timeEnd)
                         )
-                    } else {
-                        openDialog.value = true
-                    }*/
+                    }
 
                 },
                 modifier = Modifier
@@ -133,16 +130,42 @@ fun CutAnAudioScreen(
                 contentPadding = PaddingValues(20.dp, 12.dp),
 
                 ) {
-                Text(
+                pythonFlaskApiViewModel.isScopeCompleted.value?.let { isScopeCompleted ->
+                    if (!isScopeCompleted) {
+                        openDialogProcessing.value = true
+                        Text(
+                            "Procesando...",
+                            maxLines = 2,
+                            style = DLChordsTheme.typography.button,
+                            color = DLChordsTheme.colors.surface
+                        )
+                    } else {
+
+                        var response = pythonFlaskApiViewModel.responseUploadAudio?.value
+                            ?: "RESPONSE NULL DESDE PREDICCION CUT AUDIO"
+                        openDialogProcessing.value = false //cerrar el progressIndicator
+                        pdfScreenIntent.putExtra("response", response)
+
+                        //datos del audio
+                        pdfScreenIntent.putExtra("AudioProc_id", audio.id)
+                        pdfScreenIntent.putExtra("AudioProc_displayName", audio.displayName)
+                        pdfScreenIntent.putExtra("AudioProc_artist", audio.artist)
+                        pdfScreenIntent.putExtra("AudioProc_data", audio.data)
+                        pdfScreenIntent.putExtra("AudioProc_duration", audio.duration)
+                        pdfScreenIntent.putExtra("AudioProc_title", audio.title)
+
+                        //lanzamos actividad
+                        ContextCompat.startActivity(context, pdfScreenIntent, null)
+                    }
+                } ?: Text(
                     text = "PROCESAR SELECCION",
                     style = DLChordsTheme.typography.button,
                     maxLines = 1,
                     color = DLChordsTheme.colors.surface
                 )
+
             }
         }
 
     }
 }
-
-
